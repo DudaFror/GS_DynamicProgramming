9b363ad46089d60b5559e6a7462767c38144a10c
DudaFror <dudacastrocs@gmail.com>
2026-06-09 13:58:18 -0300

    feat: implement graph instance generator

---
+++ src/generate_data.py
@@ -0,0 +1,61 @@
+import random
+
+from src.data_structures import Graph
+
+
+def generate_graph(
+    num_vertices,
+    max_weight=20
+):
+
+    graph = Graph()
+
+    vertices = [
+        f"V{i}"
+        for i in range(num_vertices)
+    ]
+
+    # Garantir conectividade
+
+    for i in range(num_vertices - 1):
+
+        weight = random.randint(
+            1,
+            max_weight
+        )
+
+        graph.add_edge(
+            vertices[i],
+            vertices[i + 1],
+            weight
+        )
+
+    # Adicionar conexões extras
+
+    extra_edges = num_vertices
+
+    for _ in range(extra_edges):
+
+        source = random.choice(vertices)
+        destination = random.choice(vertices)
+
+        if source != destination:
+
+            weight = random.randint(
+                1,
+                max_weight
+            )
+
+            graph.add_edge(
+                source,
+                destination,
+                weight
+            )
+
+    return graph
+
+if __name__ == "__main__":
+
+    graph = generate_graph(5)
+
+    graph.display()
